fdfe27d4886a1745ae97a9ce974867d1f4a49a2f
alexg <alejandrogomezpublic@gmail.com>
2026-01-16 22:55:08 +0100

    correccion de estructura

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,26 @@ from app.models import init_db
 from app.auth import auth, get_user_by_id
 from app.routes import routes
 from app.config import SECRET_KEY
+import os
 
 def create_app():
-    app = Flask(__name__)
-    app.secret_key = SECRET_KEY
+    base_dir = os.path.abspath(os.path.dirname(__file__))
 
+    app = Flask(
+        __name__,
+        template_folder=os.path.join(base_dir, "templates"),
+        static_folder=os.path.join(base_dir, "static")
+    )
+
+    app.secret_key = SECRET_KEY
     init_db()
 
-    login = LoginManager(app)
+    login = LoginManager()
+    login.init_app(app)
     login.login_view = "auth.login"
 
     @login.user_loader
-    def load_user(user_id) :
+    def load_user(user_id):
         return get_user_by_id(user_id)
 
     app.register_blueprint(auth)
