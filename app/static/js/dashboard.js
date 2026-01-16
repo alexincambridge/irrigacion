58c0e8c2708511901b1cc2f8ec5b05a09f67517f
alexg <alejandrogomezpublic@gmail.com>
2026-01-16 23:43:31 +0100

    gauges added

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,35 +1,3 @@
-const tempCtx = document.getElementById("tempChart")
-const humCtx = document.getElementById("humChart")
-
-const tempChart = new Chart(tempCtx, {
-  type: 'line',
-  data: { labels: [], datasets: [{ label: '°C', data: [], tension: 0.4 }] }
-})
-
-const humChart = new Chart(humCtx, {
-  type: 'line',
-  data: { labels: [], datasets: [{ label: '%', data: [], tension: 0.4 }] }
-})
-
-function updateCharts(){
-  fetch('/history')
-    .then(r => r.json())
-    .then(data => {
-      tempChart.data.labels = data.map(d => d.time)
-      tempChart.data.datasets[0].data = data.map(d => d.temperature)
-
-      humChart.data.labels = data.map(d => d.time)
-      humChart.data.datasets[0].data = data.map(d => d.humidity)
-
-      tempChart.update()
-      humChart.update()
-    })
-}
-
-
-setInterval(updateCharts, 5000)
-updateCharts()
-
 // ===== GAUGES =====
 const tempGauge = new ApexCharts(document.querySelector("#tempGauge"), {
   chart: { type: 'radialBar', height: 220 },
