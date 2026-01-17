e883e08d4a721dd2da3eb079bd08b64e77175d6f
alexg <alejandrogomezpublic@gmail.com>
2026-01-17 22:40:53 +0100

    customising

--- app/models.py
+++ app/models.py
@@ -13,35 +13,6 @@ def get_db():
         g.db.execute("PRAGMA journal_mode=WAL;")
     return g.db
 
-
-def init_db():
-    db = get_db()
-    c = db.cursor()
-
-    c.execute("""
-    CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY,
-        username TEXT UNIQUE,
-        password TEXT
-    )""")
-
-    c.execute("""
-    CREATE TABLE IF NOT EXISTS sensor_data(
-        id INTEGER PRIMARY KEY,
-        temperature REAL,
-        humidity REAL,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-    )""")
-
-    c.execute("""
-    CREATE TABLE IF NOT EXISTS irrigation_log(
-        id INTEGER PRIMARY KEY,
-        mode TEXT,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-    )""")
-
-    db.commit()
-
 def close_db(e=None):
     db = g.pop("db", None)
     if db:
--- app/scheduler.py
+++ app/scheduler.py
@@ -1,17 +1,21 @@
 import time
+import sqlite3
 from app.hardware import read_dht
-from app.models import get_db
+
+DB_PATH = "database/irrigation.db"
 
 def scheduler_loop():
     while True:
         t, h = read_dht()
 
         if t is not None and h is not None:
-            db = get_db()
+            db = sqlite3.connect(DB_PATH, timeout=5)
+            db.execute("PRAGMA journal_mode=WAL;")
             db.execute(
                 "INSERT INTO sensor_data (temperature, humidity) VALUES (?, ?)",
                 (t, h)
             )
             db.commit()
+            db.close()
 
         time.sleep(60)
