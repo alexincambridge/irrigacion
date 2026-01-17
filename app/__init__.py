4631f437793470eeef7071d0275a6c0380db96bc
alexg <alejandrogomezpublic@gmail.com>
2026-01-17 22:24:31 +0100

    customising

--- app/__init__.py
+++ app/__init__.py
@@ -1,37 +1,20 @@
 from flask import Flask
 from flask_login import LoginManager
-from app.models import init_db
-from app.auth import auth, get_user_by_id
+from app.models import close_db
 from app.routes import routes
-from app.config import SECRET_KEY
-import os
-
+from app.auth import auth
 
 def create_app():
-    base_dir = os.path.abspath(os.path.dirname(__file__))
+    app = Flask(__name__)
+    app.secret_key = "change-this"
 
-    app = Flask(
-        __name__,
-        template_folder=os.path.join(base_dir, "templates"),
-        static_folder=os.path.join(base_dir, "static")
-    )
-
-
-    app.secret_key = SECRET_KEY
-    init_db()
-
-    login = LoginManager()
-    login.init_app(app)
-    login.login_view = "auth.login"
-
-    @login.user_loader
-    def load_user(user_id):
-        return get_user_by_id(user_id)
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
 
     app.register_blueprint(auth)
     app.register_blueprint(routes)
 
+    app.teardown_appcontext(close_db)
+
     return app
-
-app.teardown_appcontext(close_db)
-
