4e9c57f022d08a5ba8a286fb0f6f7af5e341abe0
alexg <alejandrogomezpublic@gmail.com>
2026-01-17 22:43:03 +0100

    customising

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from app.models import close_db
 from app.routes import routes
-from app.auth import auth
+from app.auth import auth, get_user_by_id
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +11,10 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return get_user_by_id(user_id)
 
     app.register_blueprint(auth)
     app.register_blueprint(routes)
