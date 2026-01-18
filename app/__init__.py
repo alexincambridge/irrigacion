df5553aad5a87c3789ccdfcb992d00fc4f39dad3
alexg <alejandrogomezpublic@gmail.com>
2026-01-18 18:56:17 +0100

    hora y fecha

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager
 from app.models import close_db
 from app.routes import routes
 from app.auth import auth, get_user_by_id
+from datetime import datetime
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +12,14 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
+
+    @app.context_processor
+    def inject_datetime() :
+        now = datetime.now()
+        return {
+            "now" : now,
+            "now_str" : now.strftime("%H:%M · %d/%m/%Y")
+        }
 
     @login_manager.user_loader
     def load_user(user_id):
