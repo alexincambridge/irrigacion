e08de20ae32e3a6e5d602aacba3c0f607d347f16
alexg <alejandrogomezpublic@gmail.com>
2026-01-18 23:43:58 +0100

    scheduler updated

--- run.py
+++ run.py
@@ -4,7 +4,10 @@ import threading
 
 app = create_app()
 
-threading.Thread(target=scheduler_loop, daemon=True).start()
+threading.Thread(
+    target=scheduler_loop,
+    daemon=True
+).start()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", debug=True)
