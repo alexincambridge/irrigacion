f7808e4f77b2b8f850b4b3a2de2d4c49c9f6dfd1
alexg <alejandrogomezpublic@gmail.com>
2026-01-19 22:50:23 +0100

    bd updated

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
 from app.scheduler import scheduler_loop
 import threading
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, "instance", "irrigation.db")
 
 app = create_app()
 
