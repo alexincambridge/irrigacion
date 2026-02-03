aa5535cf475b35c800687c69355c0d913d53c48d
alexg <alejandrogomezpublic@gmail.com>
2026-02-03 17:19:50 +0100

    dht11 and dashboard set up with db

--- app/routes.py
+++ app/routes.py
@@ -9,35 +9,6 @@ routes = Blueprint("routes", __name__)
 def dashboard():
     return render_template("dashboard.html")
 
-@routes.route("/dashboard/data")
-@login_required
-def dashboard_data():
-    db = get_db()
-
-    sensor = db.execute("""
-        SELECT temperature, humidity, solar, pressure, ec, ph, timestamp
-        FROM sensor_data
-        ORDER BY timestamp DESC
-        LIMIT 1
-    """).fetchone()
-
-    water = db.execute("""
-        SELECT SUM(liters), SUM(cost)
-        FROM water_consumption
-    """).fetchone()
-
-    return jsonify({
-        "temperature": sensor["temperature"],
-        "humidity": sensor["humidity"],
-        "solar": sensor["solar"],
-        "pressure": sensor["pressure"],
-        "ec": sensor["ec"],
-        "ph": sensor["ph"],
-        "time": sensor["timestamp"],
-        "water_liters": water[0] or 0,
-        "water_cost": water[1] or 0
-    })
-
 
 @routes.route("/water")
 @login_required
