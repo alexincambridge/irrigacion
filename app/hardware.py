be51f780b2e38b42fc48593a41222e3ca043bbf3
alexg <alejandrogomezpublic@gmail.com>
2026-02-15 20:23:40 +0100

    simple program irrigation

--- app/hardware.py
+++ app/hardware.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 ZONE_PINS = {
     1: 23,
@@ -12,11 +13,28 @@ for pin in ZONE_PINS.values():
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.LOW)
 
+
 def zone_on(zone_id):
-    GPIO.output(ZONE_PINS[zone_id], GPIO.HIGH)
+    pin = ZONE_PINS.get(zone_id)
+    if pin:
+        GPIO.output(pin, GPIO.HIGH)
+        print(f"GPIO {pin} ON")
+
 
 def zone_off(zone_id):
-    GPIO.output(ZONE_PINS[zone_id], GPIO.LOW)
+    pin = ZONE_PINS.get(zone_id)
+    if pin:
+        GPIO.output(pin, GPIO.LOW)
+        print(f"GPIO {pin} OFF")
+
 
 def zone_state(zone_id):
-    return GPIO.input(ZONE_PINS[zone_id]) == GPIO.HIGH
+    pin = ZONE_PINS.get(zone_id)
+    if pin:
+        return GPIO.input(pin) == GPIO.HIGH
+    return False
+
+
+def irrigation_off():
+    for pin in ZONE_PINS.values():
+        GPIO.output(pin, GPIO.LOW)
