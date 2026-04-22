9144144c7376c50d35e48da0d009698a760a4951
alexg <alejandrogomezpublic@gmail.com>
2026-04-22 19:27:18 +0200

    reles low to high

--- app/hardware.py
+++ app/hardware.py
@@ -15,9 +15,9 @@ PUMP_PIN = 17  # Changed to 17
 
 # ACTIVE LOW CONFIGURATION
 # Set to True if your relays turn ON with LOW and OFF with HIGH (typical for 4-channel relay modules)
-ACTIVE_LOW = False
-RELAY_ON = GPIO.LOW if ACTIVE_LOW else GPIO.HIGH
-RELAY_OFF = GPIO.HIGH if ACTIVE_LOW else GPIO.LOW
+ACTIVE_LOW = True
+RELAY_ON = GPIO.HIGH if ACTIVE_LOW else GPIO.HIGH
+RELAY_OFF = GPIO.LOW if ACTIVE_LOW else GPIO.HIGH
 
 _active_zones = set()
 _pump_active = False
