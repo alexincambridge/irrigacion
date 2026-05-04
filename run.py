477a1a8295f42bf83e2646ba49e700910ed24322
alexg <alejandrogomezpublic@gmail.com>
2026-05-04 19:02:43 +0200

    chatbot update interaction

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app
 from app.scheduler import scheduler_loop
+from app.telegram_bot import bot_listener
 import threading
 import os
 
@@ -12,6 +13,8 @@ threading.Thread(
     target=scheduler_loop,
     daemon=True
 ).start()
+
+bot_listener.start()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
